d336b317092fe748e6db4321acca7eaf1adc315b
parbol <pablom@cern.ch>
2024-03-13 16:40:52 +0100

    cosas

--- test/example.py
+++ test/example.py
@@ -1,12 +1,11 @@
 import cv2
 import numpy as np
 
-camera = cv2.VideoCapture(0)
+#camera = cv2.VideoCapture(0)
 
-return_value, image = camera.read()
-
+#return_value, image = camera.read()
+image = cv2.imread('caca.jpeg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
-
 gray = np.float32(gray_image)
 
 dst = cv2.cornerHarris(gray,2,3,0.04)
@@ -14,7 +13,7 @@ dst = cv2.cornerHarris(gray,2,3,0.04)
 #result is dilated for marking the corners, not important
 dst = cv2.dilate(dst,None)
  
-ret, dst = cv2.threshold(dst,0.1*dst.max(),255,0)
+ret, dst = cv2.threshold(dst,0.05*dst.max(),255,0)
 dst = np.uint8(dst)
 
 # find centroids
@@ -22,7 +21,7 @@ ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
 
 # define the criteria to stop and refine the corners
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.1)
-corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+corners = cv2.cornerSubPix(gray,np.float32(centroids),(11,11),(-1,-1),criteria)
 print(corners)
 res = np.hstack((centroids,corners))
 res = np.int0(res)
@@ -34,7 +33,7 @@ image[res[:,3],res[:,2]] = [0,255,0]
 
 cv2.imwrite('picture.png', image)
 
-del(camera)
+#del(camera)
 
 
 
---
+++ test/shai-algorithm.py
@@ -0,0 +1,33 @@
+# organizing imports 
+import cv2 
+import numpy as np 
+
+
+
+def shi_tomasi(image):
+
+    #Converting to grayscale
+    gray_img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    
+    #Specifying maximum number of corners as 1000
+    # 0.01 is the minimum quality level below which the corners are rejected
+    # 10 is the minimum euclidean distance between two corners
+    corners_img = cv2.goodFeaturesToTrack(gray_img,1000,0.01,5)
+    
+    corners_img = np.int0(corners_img)
+
+    for corners in corners_img:
+       
+        x,y = corners.ravel()
+        #Circling the corners in green
+        cv2.circle(image,(x,y),3,[0,255,0],-1)
+
+    return image
+
+if __name__=='__main__':
+
+    #camera = cv2.VideoCapture(0)
+    #return_value, image = camera.read()
+    image = cv2.imread('caca.jpeg') 
+    corners_image = shi_tomasi(image)
+    cv2.imwrite('picture.png', image)  
