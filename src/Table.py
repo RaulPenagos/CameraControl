4559e8050fe2298e8ca74a918594d10f5f9676c8
parbol <pablom@cern.ch>
2024-05-17 13:32:26 +0200

    adding points

--- src/Table.py
+++ src/Table.py
@@ -22,6 +22,48 @@ class Table:
     
     def generatePointsInSquare(self, x0, y0, z0, L):
 
+        #Network 1
+        x = [-37.5, -25.0, -12.5, 0.0, 12.5, 25.0, 37.5, 50.0]
+        y = [-50.0, -37.5, -25.0, -12.5, 0.0, 12.5, 25.0]
+        for ix in x:
+            for iy in y:
+                if iy > -25.1:
+                    continue
+                self.addReferencePoint(ix, iy, 0.0)
+        for iy in y:
+            self.addReferencePoint(50.0, iy)
+        for ix in x:
+            self.addReferencePoint(ix, 25.0)
+        for ix in x:
+            if ix > -25.1:
+                continue
+            for iy in y:
+                self.addReferencePoint(ix, iy)
+        #Horizontal Line 1
+        self.addReferencePoint(0, -60.0)
+        #Vertical Line Right
+        y2 = [-60.0, -50.0, -32.0, 24.0]
+        for iy in y2:
+            self.addReferencePoint(60.0, iy)
+        #Horizontal Line 2
+        self.addReferencePoint(-14.0, -19.0, 0.0)
+        self.addReferencePoint(14.0, -19.0, 0.0)
+        #Horizontal Line 3
+        self.addReferencePoint(-19.0, -14.0, 0.0)
+        self.addReferencePoint(14.0, -14.0, 0.0)
+
+
+
+        self.addReferencePoint(-37.5, -50.0, 0.0)
+        self.addReferencePoint(-37.5, -37.5, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+        self.addReferencePoint(-37.5, 0.0, 0.0)
+
         x1 = x0 
         y1 = y0 
         z1 = z0
